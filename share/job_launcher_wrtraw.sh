434bac4114fefcc91ac940837723b54ac46599e9
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-09-09 14:48:45 +0200

    Try.

--- share/job_launcher_wrtraw.sh
+++ share/job_launcher_wrtraw.sh
@@ -138,7 +138,7 @@ hep_sub job_worker_wrtraw.sh \
   -n "$job_count" \
   -cpu 1 \
   -m 4096 \
+  -wt short \
   -o "/scratchfs/juno/traymond/agrpc_${run_number}_%{ProcId}.log" \
   -e "/scratchfs/juno/traymond/agrpc_${run_number}_%{ProcId}.err" \
   -name agrpc_${run_number}_batch
-  # -wt default \
