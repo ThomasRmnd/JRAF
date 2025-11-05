80eec8fb2b001a09b3adbf8a540ce566e11b85ec
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-11-05 10:34:33 +0100

    Try

--- share/single_reprod_eos_cca.in2p3.fr/job_launcher.sh
+++ share/single_reprod_eos_cca.in2p3.fr/job_launcher.sh
@@ -128,7 +128,7 @@ submit_jobs() {
 
     sbatch \
         --job-name="agrpc_${RUN_NUMBER}_batch" \
-        --output="/sps/juno/jdeandre/rtraw_ThomasRaymond/analysis/log/agrpc_${RUN_NUMBER}_%a.log" \
+        --output="/dev/null" \
         --error="/sps/juno/jdeandre/rtraw_ThomasRaymond/analysis/err/agrpc_${RUN_NUMBER}_%a.err" \
         --array="0-$((JOB_COUNT - 1))" \
         --partition="htc" \
