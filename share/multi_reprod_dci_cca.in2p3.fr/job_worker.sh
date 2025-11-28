07cb9dbc8c139584e1c83494393d9bb8f782b39e
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-11-28 16:07:44 +0100

    Try

--- share/multi_reprod_dci_cca.in2p3.fr/job_worker.sh
+++ share/multi_reprod_dci_cca.in2p3.fr/job_worker.sh
@@ -129,7 +129,7 @@ include_neighbor() {
 resolve_input_paths() {
     local input_reprod_file="$1"
 
-    if [[ "$input_reprod_file" =~ /juno/juno-reprod/([^/]+)/([^/]+)/([^/]+)/([^/]+)/([^/]+)/RUN\.([0-9]+)\.[^/]*([0-9]{8})[^/]* ]]; then
+    if [[ "$input_reprod_file" =~ /juno/juno-reprod/([^/]+)/([^/]+)/([^/]+)/([^/]+)/([^/]+)/RUN\.([0-9]+)\.[^/]*([0-9]{14})[^/]* ]]; then
         campaign="${BASH_REMATCH[1]}"
         stream="${BASH_REMATCH[2]}"
         run_bucket="${BASH_REMATCH[3]}"
