df8c8cf350100b956aec90e2e01e52fd3d3ddddc
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2025-12-11 15:55:11 +0100

    Try

--- share/analysis/vertex/analysisgroupc_printer.C
+++ share/analysis/vertex/analysisgroupc_printer.C
@@ -446,28 +446,28 @@ void analysisgroupc_printer(const std::string& filename, const std::string& suff
     double e_p_width = 0.20;
     int e_p_nbin = std::round((e_p_max - e_p_min) / e_p_width) + 1;
     std::vector<double> e_p_bins = linspace_cpp(e_p_min, e_p_max, e_p_nbin);
-    TH1D* h_e_p = new TH1D("h_e_p", "Prompt energy", e_p_bins.size(), e_p_bins.data());
+    TH1D* h_e_p = new TH1D("h_e_p", "Prompt energy", e_p_bins.size() - 1, e_p_bins.data());
 
     double e_d_min = 2.0;
     double e_d_max = 2.5;
     double e_d_width = 0.02;
     int e_d_nbin = std::round((e_d_max - e_d_min) / e_d_width) + 1;
     std::vector<double> e_d_bins = linspace_cpp(e_d_min, e_d_max, e_d_nbin);
-    TH1D* h_e_d = new TH1D("h_e_d", "Delayed energy", e_d_bins.size(), e_d_bins.data());
+    TH1D* h_e_d = new TH1D("h_e_d", "Delayed energy", e_d_bins.size() - 1, e_d_bins.data());
 
     double e_dt_min = 0.0;
     double e_dt_max = 1.0;
     double e_dt_width = 0.025;
     int e_dt_nbin = std::round((e_dt_max - e_dt_min) / e_dt_width) + 1;
     std::vector<double> e_dt_bins = linspace_cpp(e_dt_min, e_dt_max, e_dt_nbin);
-    TH1D* h_dt = new TH1D("h_dt", "Prompt-Delayed time difference", e_dt_bins.size(), e_dt_bins.data());
+    TH1D* h_dt = new TH1D("h_dt", "Prompt-Delayed time difference", e_dt_bins.size() - 1, e_dt_bins.data());
 
     double e_dr_min = 0.0;
     double e_dr_max = 1.5;
     double e_dr_width = 0.05;
     int e_dr_nbin = std::round((e_dr_max - e_dr_min) / e_dr_width) + 1;
     std::vector<double> e_dr_bins = linspace_cpp(e_dr_min, e_dr_max, e_dr_nbin);
-    TH1D* h_dr = new TH1D("h_dr", "Prompt-Delayed distance", e_dr_bins.size(), e_dr_bins.data());
+    TH1D* h_dr = new TH1D("h_dr", "Prompt-Delayed distance", e_dr_bins.size() - 1, e_dr_bins.data());
 
     double rho_min = 0.0;
     double rho_max = 17.7 * 17.7;
@@ -477,8 +477,8 @@ void analysisgroupc_printer(const std::string& filename, const std::string& suff
     int z_nbin = 51;
     std::vector<double> rho_bins = linspace_cpp(rho_min, rho_max, rho_nbin);
     std::vector<double> z_bins = linspace_cpp(z_min, z_max, z_nbin);
-    TH2D* h_rho_z_p = new TH2D("h_rho_z_p", "Prompt vertex distribution", rho_bins.size(), rho_bins.data(), z_bins.size(), z_bins.data());
-    TH2D* h_rho_z_d = new TH2D("h_rho_z_d", "Delayed vertex distribution", rho_bins.size(), rho_bins.data(), z_bins.size(), z_bins.data());
+    TH2D* h_rho_z_p = new TH2D("h_rho_z_p", "Prompt vertex distribution", rho_bins.size() - 1, rho_bins.data(), z_bins.size() - 1, z_bins.data());
+    TH2D* h_rho_z_d = new TH2D("h_rho_z_d", "Delayed vertex distribution", rho_bins.size() - 1, rho_bins.data(), z_bins.size() - 1, z_bins.data());
 
     for (const IBD& ibd : ibds) {
         h_e_p->Fill(ibd.prompt.e);
