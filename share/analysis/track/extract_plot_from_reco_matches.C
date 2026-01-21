8eabf250c91bd553e323e85470c19c807b6e4c4f
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-01-21 15:31:00 +0100

    Try

--- share/analysis/track/extract_plot_from_reco_matches.C
+++ share/analysis/track/extract_plot_from_reco_matches.C
@@ -121,7 +121,7 @@ void extract_plot_from_reco_matches(const char* filename) {
     };
 
     for (int k = 0; k < tree->GetEntries(); ++k) {
-        // if (k % 1000 == 0) std::cout << "\rEntries: " << k << " / " << tree->GetEntries();
+        if (k % 1000 == 0) std::cout << "\rEntries: " << k << " / " << tree->GetEntries();
         tree->GetEntry(k);
 
         if (NTotPoints == 0) continue;
