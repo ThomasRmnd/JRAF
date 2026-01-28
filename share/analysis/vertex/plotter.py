496e4b70b4f8c44961de93d71953d6d4f455a1fc
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-01-28 17:00:01 +0100

    Try

--- share/analysis/vertex/plotter.py
+++ share/analysis/vertex/plotter.py
@@ -103,7 +103,7 @@ def ibd_analysis_plot(filepath: str):
         zorder=3
     )
 
-    ax.set_xlabel(r"$E_{p} (MeV)")
+    ax.set_xlabel(r"$E_{p}$ (MeV)")
     ax.set_ylabel(r"Entries")
 
     ax.tick_params(direction="in", which="both", top=True, right=True)
