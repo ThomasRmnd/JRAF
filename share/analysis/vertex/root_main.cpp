38a84dd35aaf9e1829c3bf04ba2d0ac6843bf97c
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-03 16:31:07 +0100

    Try

--- share/analysis/vertex/root_main.cpp
+++ share/analysis/vertex/root_main.cpp
@@ -113,7 +113,7 @@ void save_meta_info(const std::string& filename) {
 int root_main(const std::string& filepath) {
     std::string suffix = "__OMILREC_JVtx";
 
-    save_meta_info(filepath);
+    // save_meta_info(filepath);
 
     analysis_registry registry;
     analysis_manager manager(registry);
