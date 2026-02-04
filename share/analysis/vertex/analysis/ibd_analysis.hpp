b74f4ccd976582edfa7a74ed3c31097fad96ceeb
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-04 11:06:56 +0100

    Try

--- share/analysis/vertex/analysis/ibd_analysis.hpp
+++ share/analysis/vertex/analysis/ibd_analysis.hpp
@@ -24,36 +24,41 @@ public:
     ~ibd_analysis() override = default;
 
     bool selection() override {
-        if (m_nav->meta_prompt.stdt > 200.0 || m_nav->meta_delayed.stdt > 200.0) return false; // Flasher cut
-        if (mag(m_nav->prompt.pos) > 16500.0) return false; // Fiducial cut
-        if ((m_nav->prompt.pos.z < -15500.0 || 15500 < m_nav->prompt.pos.z) && std::sqrt(m_nav->prompt.pos.x * m_nav->prompt.pos.x + m_nav->prompt.pos.y * m_nav->prompt.pos.y) < 3000.0) return false; // Chimney cut
-        if (m_nav->prompt.e < 0.7 || 12.0 < m_nav->prompt.e) return false; // Prompt energy cut
-        if (m_nav->delayed.e < 2.0 || 2.5 < m_nav->delayed.e) return false; // Delayed energy cut
+        if (m_nav->prompt.e < 0.7 || 12.0 < m_nav->prompt.e) return false;
+        if (m_nav->delayed.e < 2.0 || 2.5 < m_nav->delayed.e) return false;
+        if (mag(m_nav->prompt.pos) > 16500.0) return false;
+        if (std::abs(m_nav->prompt.pos.z) > 15500.0 && std::sqrt(m_nav->prompt.pos.x * m_nav->prompt.pos.x + m_nav->prompt.pos.y * m_nav->prompt.pos.y) < 2000.0) return false;
+        timestamp ts_diff = m_nav->delayed.ts - m_nav->prompt.ts;
+        if (ts_diff < timestamp{0, 5000} || timestamp{0, 1000000} < ts_diff) return false;
+        vec3 pos_diff = m_nav->delayed.pos - m_nav->prompt.pos;
+        if (mag(pos_diff) > 1500.0) return false;
 
         std::size_t nb_multu_veto = 0ul;
         for (std::size_t k = 0ul; k < m_nav->e_mult.size(); ++k) {
             if (m_nav->e_mult[k] < 2.0 || 12.0 < m_nav->e_mult[k]) continue;
             timestamp ts_mult{m_nav->sec_mult[k], m_nav->nsec_mult[k]};
             vec3 pos_mult{m_nav->posx_mult[k], m_nav->posy_mult[k], m_nav->posz_mult[k]};
-            // if (pos_mult.Mag() > 17700.0) continue;
-            // if ((pos_p - pos_n).Mag() > 4000.0 || (pos_p - pos_n).Mag() > 4000.0) continue;
             if (ts_mult < m_nav->prompt.ts - timestamp{0, 1000000} || m_nav->delayed.ts + timestamp{0, 1000000} < ts_mult) continue;
             ++nb_multu_veto;
         }
+        if (nb_multu_veto) return false;
 
         std::size_t nb_neutron_veto = 0ul;
         for (std::size_t k = 0ul; k < m_nav->e_n.size(); ++k) {
             if (m_nav->e_n[k] < 1.5 || 20.0 < m_nav->e_n[k]) continue;
+            if (m_nav->stdt_n[k] > 275.0) continue;
             timestamp ts_n{m_nav->sec_n[k], m_nav->nsec_n[k]};
             vec3 pos_n{m_nav->posx_n[k], m_nav->posy_n[k], m_nav->posz_n[k]};
-            // if (pos_n.Mag() > 17700.0) continue;
             if (mag(m_nav->prompt.pos - pos_n) > 4000.0 || mag(m_nav->delayed.pos - pos_n) > 4000.0) continue;
             if (m_nav->prompt.ts < ts_n + timestamp{0, 20000} || ts_n + timestamp{0, 1200000000} < m_nav->prompt.ts) continue;
             if (m_nav->delayed.ts < ts_n + timestamp{0, 20000} || ts_n + timestamp{0, 1200000000} < m_nav->delayed.ts) continue;
             ++nb_neutron_veto;
         }
+        if (nb_neutron_veto) return false;
 
-        return (nb_neutron_veto == 0ul && nb_multu_veto == 0ul);
+        if ( std::pow((m_nav->meta_prompt.stdhit - 0.55) / 0.45, 2.0) + std::pow((m_nav->meta_prompt.stdt - 170.0) / 80.0, 2.0) <= 1 ) return false;
+
+        return true;
     }
 
     bool process() override {
