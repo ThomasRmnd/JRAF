0fbfd86548ff7617d943eff48d746b9d43a8cb9b
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-05 14:08:46 +0100

    Try

--- share/analysis/track/muon_rate.py
+++ share/analysis/track/muon_rate.py
@@ -120,9 +120,9 @@ def calculate_muon_rate(filepath : str):
     mask_cd_only = np.logical_and(mask_cd, np.logical_not(mask_wp))
     mask_wp_only = np.logical_and(np.logical_not(mask_cd), mask_wp)
 
-    data_cd_wp = data[mask_cd_wp]
-    data_cd_only = data[mask_cd_only]
-    data_wp_only = data[mask_wp_only]
+    data_cd_wp = {key: val[mask_cd_wp] for key, val in data.items()}
+    data_cd_only = {key: val[mask_cd_only] for key, val in data.items()}
+    data_wp_only = {key: val[mask_wp_only] for key, val in data.items()}
 
     ts_cd_wp = np.array([timestamp(sec, nsec) for sec, nsec in zip(data_cd_wp["sec"], data_cd_wp["nsec"])])
     ts_cd_only = np.array([timestamp(sec, nsec) for sec, nsec in zip(data_cd_only["sec"], data_cd_only["nsec"])])
