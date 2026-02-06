a8ac025deed672dde1630828c9224c7a468b4f04
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-06 11:11:49 +0100

    Try

--- share/analysis/track/fast_muon_reconstruction_comparison.C
+++ share/analysis/track/fast_muon_reconstruction_comparison.C
@@ -3,6 +3,35 @@
 #include <TFile.h>
 #include <TTimeStamp.h>
 #include <TTree.h>
+#include <TVector3.h>
+
+struct track {
+
+    int run_id;
+    TTimeStamp ts;
+    double totq_cd;
+    double totq_wp;
+
+    TVector3 ipos;
+    TVector3 fpos;
+
+};
+
+bool operator<(const track& a, const track& b) {
+    return a.ts < b.ts;
+}
+
+double compute_angle_between_track(const track& a, const track& b) {
+    TVector3 dir_a = (a.fpos - a.ipos).Unit();
+    TVector3 dir_b = (b.fpos - b.ipos).Unit();
+    return dir_a.Angle(dir_b) * 180.0 / M_PI;
+}
+
+double compute_distance_between_track(const track& a, const track& b) {
+    TVector3 mpos_a = (a.ipos + a.fpos) * 0.5;
+    TVector3 mpos_b = (b.ipos + b.fpos) * 0.5;
+    return (mpos_a - mpos_b).Mag() / 1000.0;
+}
 
 double get_quantile(std::vector<double>::const_iterator first, std::vector<double>::const_iterator last, double quantile) {
     if (first == last) return 0.0;
@@ -43,17 +72,95 @@ void plot_metrics(std::vector<TH1D*> hists) {
     c->Update();
 }
 
-int fast_muon_reconstruction_comparison(const char* filepath) {
-    TFile* file = TFile::Open(filepath, "READ");
-    if (!file) {
-        std::cerr << "Cannot open file " << filepath << '\n';
-        return 1;
+std::set<track> open_amber_v5_5_user_chain(const char* path) {
+    TChain* chain = new TChain("MuonReco");
+    chain->Add(path);
+    std::set<track> tracks;
+
+    Int32_t runID;
+    Int32_t eventID;
+    Int32_t fSec;
+    Int32_t fNanoSec;
+    Int32_t muonType;
+    Float32_t xin, yin, zin;
+    Float16_t xout, yout, zout;
+    Float32_t charge;
+    chain->SetBranchAddress("runID", &runID);
+    chain->SetBranchAddress("eventID", &eventID);
+    chain->SetBranchAddress("fSec", &fSec);
+    chain->SetBranchAddress("fNanoSec", &fNanoSec);
+    chain->SetBranchAddress("muonType", &muonType);
+    chain->SetBranchAddress("xin", &xin);
+    chain->SetBranchAddress("yin", &yin);
+    chain->SetBranchAddress("zin", &zin);
+    chain->SetBranchAddress("xout", &xout);
+    chain->SetBranchAddress("yout", &yout);
+    chain->SetBranchAddress("zout", &zout);
+
+    long nentries = chain->GetEntries();
+    for (long k = 0l; k < nentries; ++k) {
+        chain->GetEntry(k);
+        tracks.push_back(track{
+            .run_id = runID,
+            .ts = TTimeStamp(fSec, fNanoSec),
+            .totq_cd = 0.0,
+            .totq_wp = charge,
+            .ipos = TVector3(xin, yin, zin),
+            .fpos = TVector3(xout, yout, zout)
+        });
     }
-    TTree* tree = file->Get<TTree>("muons");
-    if (!tree) {
-        std::cerr << "Cannot retrieve tree muons in file " << filepath << '\n';
-        return 1;
+    return tracks;
+}
+
+std::set<track> open_edwin_user_chain(const char* path) {
+    TChain* chain = new TChain("Single_Reco");
+    chain->Add(path);
+    std::set<track> tracks;
+
+    Int32_t run_number;
+    Int32_t cd_file;
+    Int32_t cd_time_s;
+    Int32_t cd_time_ns;
+    Float32_t cd_totalPE;
+    Float32_t enterX, enterY, enterZ;
+    Float16_t exitX, exitY, exitZ;
+    Int32_t wp_time_s;
+    Int32_t wp_time_ns;
+    Float32_t wp_totalPE;
+    chain->SetBranchAddress("run_number", &run_number);
+    chain->SetBranchAddress("cd_file", &cd_file);
+    chain->SetBranchAddress("cd_time_s", &cd_time_s);
+    chain->SetBranchAddress("cd_time_ns", &cd_time_ns);
+    chain->SetBranchAddress("cd_totalPE", &cd_totalPE);
+    chain->SetBranchAddress("enterX", &enterX);
+    chain->SetBranchAddress("enterY", &enterY);
+    chain->SetBranchAddress("enterZ", &enterZ);
+    chain->SetBranchAddress("exitX", &exitX);
+    chain->SetBranchAddress("exitY", &exitY);
+    chain->SetBranchAddress("exitZ", &exitZ);
+    chain->SetBranchAddress("wp_time_s", &wp_time_s);
+    chain->SetBranchAddress("wp_time_ns", &wp_time_ns);
+    chain->SetBranchAddress("wp_totalPE", &wp_totalPE);
+
+    long nentries = chain->GetEntries();
+    for (long k = 0l; k < nentries; ++k) {
+        chain->GetEntry(k);
+        tracks.push_back(track{
+            .run_id = run_number,
+            .ts = TTimeStamp(cd_time_s, cd_time_ns),
+            .totq_cd = cd_totalPE,
+            .totq_wp = wp_totalPE,
+            .ipos = TVector3(enterX, enterY, enterZ),
+            .fpos = TVector3(exitX, exitY, exitZ)
+        });
     }
+    return tracks;
+}
+
+std::map<std::string, std::set<track>> open_joint_reco_user_chain(const char* path) {
+    TChain* chain = new TChain("muons");
+    chain->Add(path);
+    std::map<std::string, std::set<track>> tracks;
 
     int run_id;
     time_t sec;
@@ -65,20 +172,137 @@ int fast_muon_reconstruction_comparison(const char* filepath) {
     std::vector<double> *quality = nullptr;
     std::vector<double> *iposx = nullptr, *iposy = nullptr, *iposz = nullptr; 
     std::vector<double> *fposx = nullptr, *fposy = nullptr, *fposz = nullptr;
-    tree->SetBranchAddress("run_id", &run_id);
-    tree->SetBranchAddress("sec", &sec);
-    tree->SetBranchAddress("nsec", &nsec);
-    // tree->SetBranchAddress("totq_cd", &totq_cd);
-    // tree->SetBranchAddress("totq_wp", &totq_wp);
-    tree->SetBranchAddress("method", &method);
-    tree->SetBranchAddress("det", &det);
-    tree->SetBranchAddress("quality", &quality);
-    tree->SetBranchAddress("iposx", &iposx);
-    tree->SetBranchAddress("iposy", &iposy);
-    tree->SetBranchAddress("iposz", &iposz);
-    tree->SetBranchAddress("fposx", &fposx);
-    tree->SetBranchAddress("fposy", &fposy);
-    tree->SetBranchAddress("fposz", &fposz);
+    chain->SetBranchAddress("run_id", &run_id);
+    chain->SetBranchAddress("sec", &sec);
+    chain->SetBranchAddress("nsec", &nsec);
+    chain->SetBranchAddress("totq_cd", &totq_cd);
+    chain->SetBranchAddress("totq_wp", &totq_wp);
+    chain->SetBranchAddress("method", &method);
+    chain->SetBranchAddress("det", &det);
+    chain->SetBranchAddress("quality", &quality);
+    chain->SetBranchAddress("iposx", &iposx);
+    chain->SetBranchAddress("iposy", &iposy);
+    chain->SetBranchAddress("iposz", &iposz);
+    chain->SetBranchAddress("fposx", &fposx);
+    chain->SetBranchAddress("fposy", &fposy);
+    chain->SetBranchAddress("fposz", &fposz);
+
+    long nentries = chain->GetEntries();
+    for (long k = 0l; k < nentries; ++k) {
+        chain->GetEntry(k);
+        bool has_tt_info = false;
+        int ntracks_cdclassify = 0;
+        int ntracks_wpclassify = 0;
+        for (std::size_t i = 0ul; i < method->size(); ++i) {
+            if ((*method)[i] == "Tt") {
+                has_tt_info = true;
+            }
+            if ((*method)[i] == "CdClassify") {
+                ++ntracks_cdclassify;
+            }
+            if ((*method)[i] == "WpBasic") {
+                ++ntracks_wpclassify;
+            }
+        }
+        if (!has_tt_info) continue;
+        if (ntracks_cdclassify != 1) continue;
+        if (ntracks_wpclassify != 1) continue;
+        for (std::size_t i = 0ul; i < method->size(); ++i) {
+            tracks[(*method)[i]].push_back(track{
+                .run_id = run_id,
+                .ts = TTimeStamp(sec, nsec),
+                .totq_cd = totq_cd,
+                .totq_wp = totq_wp,
+                .ipos = TVector3((*iposx)[i], (*iposy)[i], (*iposz)[i]),
+                .fpos = TVector3((*fposx)[i], (*fposy)[i], (*fposz)[i])
+            });
+        }
+    }
+    return tracks;
+}
+
+std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>> compute_correlations(std::map<std::string, std::set<track>>& tracks) {
+    if (tracks.find("Tt") == tracks.end()) {
+        std::cerr << "Error: Tt tracks not found in map.\n";
+        return;
+    }
+    const std::set<track>& tt_tracks = tracks["Tt"];
+
+    std::map<std::string, std::vector<double>> angles;
+    std::map<std::string, std::vector<double>> distances;
+
+    for (const auto& [method, track_set] : tracks) {
+        if (method == "Tt") continue;
+
+        std::cout << "\n--- Correlating " << method << " with Tt ---" << std::endl;
+
+        for (const track& trk : track_set) {
+            TTimeStamp lower_bound_ts, upper_bound_ts;
+            lower_bound_ts.Set(trk.ts.GetSec(), trk.ts.GetNano() - 1000);
+            upper_bound_ts.Set(trk.ts.GetSec(), trk.ts.GetNano() + 1000);
+            
+            std::set<track>::const_iterator it_tt = tt_tracks.lower_bound({0, lower_bound_ts, 0, 0, {}, {}});
+
+            while (it != track_set.end() && lower_bound_ts <= it_tt->ts && it_tt->ts <= upper_bound_ts) {
+                angles[method].push_back(ompute_angle_between_track(trk, *it_tt));
+                distances[method].push_back(compute_distance_between_track(trk, *it_tt));
+                ++it_tt;
+            }
+        }
+    }
+    return std::make_pair(angles, distances);
+}
+
+std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>> compute_global_correlations(std::map<std::string, std::set<track>>& tracks) {
+    if (tracks.find("Tt") == tracks.end()) {
+        std::cerr << "Error: Tt tracks not found in map.\n";
+        return;
+    }
+    const std::set<track>& tt_tracks = tracks["Tt"];
+
+    std::map<std::string, std::vector<double>> angles;
+    std::map<std::string, std::vector<double>> distances;
+
+    for (const track& tt_muon : tt_tracks) {
+        std::map<std::string, track> coincident_map;
+        bool all_found = true;
+
+        for (const auto& [method, track_set] : tracks) {
+            if (method == "Tt") continue;
+            bool found_in_method = false;
+            TTimeStamp lower_bound_ts, upper_bound_ts;
+            lower_bound_ts.Set(tt_muon.ts.GetSec(), tt_muon.ts.GetNano() - 1000);
+            upper_bound_ts.Set(tt_muon.ts.GetSec(), tt_muon.ts.GetNano() + 1000);
+            auto it = track_set.lower_bound({0, lower_bound_ts, 0, 0, {}, {}});
+            
+            while (it != track_set.end() && lower_bound_ts <= it->ts && it->ts <= upper_bound_ts) {
+                coincident_map[method] = *it;
+                found_in_method = true;
+                break;
+            }
+
+            if (!found_in_method) {
+                all_found = false;
+                break; 
+            }
+        }
+
+        if (all_found) {
+            angles[method].push_back(ompute_angle_between_track(*it, tt_muon));
+            distances[method].push_back(compute_distance_between_track(*it, tt_muon));
+        }
+    }
+    return std::make_pair(angles, distances);
+}
+
+int fast_muon_reconstruction_comparison(const char* path_joint, const char* path_amber, const char* path_edwin) {
+    std::map<std::string, std::set<track>> tracks = open_joint_reco_user_chain(path_joint);
+    tracks["Amber_v5.5"] = open_amber_v5_5_user_chain(path_amber);
+    tracks["Edwin"] = open_edwin_user_chain(path_edwin);
+
+    std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>>> correlations = compute_correlations(tracks);
+    const std::map<std::string, std::vector<double>>& angles = correlations.first;
+    const std::map<std::string, std::vector<double>>& distances = correlations.second;
 
     std::map<std::string, TH1D*> method_angle_map;
     std::map<std::string, TH1D*> method_distance_map;
@@ -89,56 +313,26 @@ int fast_muon_reconstruction_comparison(const char* filepath) {
     method_distance_map["CdClassify"] = new TH1D("h_distance_cdclassify", "Distance between tracks middle point (CdClassify);d_{mid} (m);Entries;", 25, 0.0, 2.0);
     method_angle_map["WpBasic"] = new TH1D("h_angle_wpclassify", "Angle between tracks direction (WpClassify);#alpha (deg);Entries;", 25, 0.0, 5.0);
     method_distance_map["WpBasic"] = new TH1D("h_distance_wpclassify", "Distance between tracks middle point (WpClassify);d_{mid} (m);Entries;", 25, 0.0, 2.0);
+    method_angle_map["Amber_v5.5"] = new TH1D("h_angle_amber", "Angle between tracks direction (Amber);#alpha (deg);Entries;", 25, 0.0, 5.0);
+    method_distance_map["Amber_v5.5"] = new TH1D("h_distance_amber", "Distance between tracks middle point (Amber);d_{mid} (m);Entries;", 25, 0.0, 2.0);
+    method_angle_map["Edwin"] = new TH1D("h_angle_edwin", "Angle between tracks direction (Edwin);#alpha (deg);Entries;", 25, 0.0, 5.0);
+    method_distance_map["Edwin"] = new TH1D("h_distance_edwin", "Distance between tracks middle point (Edwin);d_{mid} (m);Entries;", 25, 0.0, 2.0);
 
-    std::vector<double> angles;
-    std::vector<double> distances;
-
-    long nentries = tree->GetEntries();
-    for (long k = 0l; k < nentries; ++k) {
-        tree->GetEntry(k);
-        std::map<std::string, std::vector<std::size_t>> track_method_map;
-        for (std::size_t i = 0ul; i < method->size(); ++i) {
-            track_method_map[(*method)[i]].push_back(i);
+    for (const auto& [method, ang] : angles) {
+        for (std::vector<double>::const_iterator it = ang.begin(); it != ang.end(); ++it) {
+            method_angle_map[method]->Fill(*it);
         }
-        if (track_method_map.find("CdWpTtChi2") == track_method_map.end()) continue;
-        if (track_method_map.find("CdClassify") == track_method_map.end()) continue;
-        if (track_method_map.find("Tt") == track_method_map.end()) continue;
-
-        if (track_method_map["Tt"].size() != 1) continue;
-        if (track_method_map["CdClassify"].size() != 1) continue;
-
-        std::size_t k_tt = track_method_map["Tt"][0];
-        TVector3 ipos_tt((*iposx)[k_tt], (*iposy)[k_tt], (*iposz)[k_tt]);
-        TVector3 fpos_tt((*fposx)[k_tt], (*fposy)[k_tt], (*fposz)[k_tt]);
-        TVector3 dir_tt = (fpos_tt - ipos_tt).Unit();
-        TVector3 mpos_tt = (ipos_tt + fpos_tt) * 0.5;
-
-        if (mpos_tt.Mag() > 17700.0) continue;
-
-        for (const auto& [key, val] : track_method_map) {
-            if (key == "Tt") continue;
-            for (std::vector<std::size_t>::const_iterator it = val.begin(); it != val.end(); ++it) {
-                TVector3 ipos((*iposx)[*it], (*iposy)[*it], (*iposz)[*it]);
-                TVector3 fpos((*fposx)[*it], (*fposy)[*it], (*fposz)[*it]);
-                TVector3 dir = (fpos - ipos).Unit();
-                TVector3 mpos = (ipos + fpos) * 0.5;
-
-                double angle = dir.Angle(dir_tt) * 180.0 / TMath::Pi();
-                double distance = (mpos - mpos_tt).Mag() / 1000.0;
-                method_angle_map[key]->Fill(angle);
-                method_distance_map[key]->Fill(distance);
-                if (key == "CdWpTtChi2") {
-                    angles.push_back(angle);
-                    distances.push_back(distance);
-                }
-            }
+    }
+    for (const auto& [method, dist] : distances) {
+        for (std::vector<double>::const_iterator it = dist.begin(); it != dist.end(); ++it) {
+            method_distance_map[method]->Fill(*it);
         }
     }
 
-    std::cout << "68.2% angle: " << get_quantile(angles.begin(), angles.end(), 0.682) << '\n';
-    std::cout << "95.4% angle: " << get_quantile(angles.begin(), angles.end(), 0.954) << '\n';
-    std::cout << "68.2% distance: " << get_quantile(distances.begin(), distances.end(), 0.682) << '\n';
-    std::cout << "95.4% distance: " << get_quantile(distances.begin(), distances.end(), 0.954) << '\n';
+    // std::cout << "68.2% angle: " << get_quantile(angles.begin(), angles.end(), 0.682) << '\n';
+    // std::cout << "95.4% angle: " << get_quantile(angles.begin(), angles.end(), 0.954) << '\n';
+    // std::cout << "68.2% distance: " << get_quantile(distances.begin(), distances.end(), 0.682) << '\n';
+    // std::cout << "95.4% distance: " << get_quantile(distances.begin(), distances.end(), 0.954) << '\n';
 
     plot_metrics({method_angle_map["CdWpTtChi2"], method_angle_map["CdClassify"], method_angle_map["WpBasic"]});
     plot_metrics({method_distance_map["CdWpTtChi2"], method_distance_map["CdClassify"], method_distance_map["WpBasic"]});
