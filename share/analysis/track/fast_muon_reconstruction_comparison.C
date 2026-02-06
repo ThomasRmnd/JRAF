a27dd93fa355fe3816c1e2c437d9b04698750b21
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-06 14:50:31 +0100

    Try

--- share/analysis/track/fast_muon_reconstruction_comparison.C
+++ share/analysis/track/fast_muon_reconstruction_comparison.C
@@ -354,18 +354,18 @@ int fast_muon_reconstruction_comparison(const char* path_joint, const char* path
     // std::cout << "95.4% distance: " << get_quantile(distances.begin(), distances.end(), 0.954) << '\n';
 
     std::vector<double> angle_quantiles = {
-        get_quantile(angles.at("CdWpTtChi2").begin(), method_angle_map.at("CdWpTtChi2").end(), 0.682),
-        get_quantile(angles.at("CdClassify").begin(), method_angle_map.at("CdClassify").end(), 0.682),
-        get_quantile(angles.at("WpBasic").begin(), method_angle_map.at("WpBasic").end(), 0.682),
-        get_quantile(angles.at("Amber_v5.5").begin(), method_angle_map.at("Amber_v5.5").end(), 0.682),
-        get_quantile(angles.at("Edwin").begin(), method_angle_map.at("Edwin").end(), 0.682)
+        get_quantile(angles.at("CdWpTtChi2")->begin(), method_angle_map.at("CdWpTtChi2")->end(), 0.682),
+        get_quantile(angles.at("CdClassify")->begin(), method_angle_map.at("CdClassify")->end(), 0.682),
+        get_quantile(angles.at("WpBasic")->begin(), method_angle_map.at("WpBasic")->end(), 0.682),
+        get_quantile(angles.at("Amber_v5.5")->begin(), method_angle_map.at("Amber_v5.5")->end(), 0.682),
+        get_quantile(angles.at("Edwin")->begin(), method_angle_map.at("Edwin")->end(), 0.682)
     };
     std::vector<double> distance_quantiles = {
-        get_quantile(method_distance_map.at("CdWpTtChi2").begin(), method_distance_map.at("CdWpTtChi2").end(), 0.682),
-        get_quantile(method_distance_map.at("CdClassify").begin(), method_distance_map.at("CdClassify").end(), 0.682),
-        get_quantile(method_distance_map.at("WpBasic").begin(), method_distance_map.at("WpBasic").end(), 0.682),
-        get_quantile(method_distance_map.at("Amber_v5.5").begin(), method_distance_map.at("Amber_v5.5").end(), 0.682),
-        get_quantile(method_distance_map.at("Edwin").begin(), method_distance_map.at("Edwin").end(), 0.682)
+        get_quantile(method_distance_map.at("CdWpTtChi2")->begin(), method_distance_map.at("CdWpTtChi2")->end(), 0.682),
+        get_quantile(method_distance_map.at("CdClassify")->begin(), method_distance_map.at("CdClassify")->end(), 0.682),
+        get_quantile(method_distance_map.at("WpBasic")->begin(), method_distance_map.at("WpBasic")->end(), 0.682),
+        get_quantile(method_distance_map.at("Amber_v5.5")->begin(), method_distance_map.at("Amber_v5.5")->end(), 0.682),
+        get_quantile(method_distance_map.at("Edwin")->begin(), method_distance_map.at("Edwin")->end(), 0.682)
     };
 
 
