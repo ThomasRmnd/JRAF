54a81bfe697ad5e0dcf59126fb07a5e46220fd2e
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-06 15:22:59 +0100

    Try

--- share/analysis/track/fast_muon_reconstruction_comparison.C
+++ share/analysis/track/fast_muon_reconstruction_comparison.C
@@ -75,7 +75,7 @@ void plot_metrics(const std::vector<TH1D*>& hists, const std::vector<double>& qu
         else {
             h->Draw("SAME");
         }
-        TLine* line = new TLine(quantiles[i], 0.0, quantiles[i], 1.0);
+        TLine* line = new TLine(quantiles[i], 0.0, quantiles[i], max * 1.1);
         line->SetLineStyle(2);
         line->SetLineWidth(3);
         line->SetLineColor(colors[i]);
