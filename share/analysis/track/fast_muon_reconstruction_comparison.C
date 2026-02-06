a523c242e1b5c0b8734e4dd44e6394f1cd203f05
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-06 15:17:06 +0100

    Try

--- share/analysis/track/fast_muon_reconstruction_comparison.C
+++ share/analysis/track/fast_muon_reconstruction_comparison.C
@@ -319,7 +319,8 @@ int fast_muon_reconstruction_comparison(const char* path_joint, const char* path
     tracks["Amber_v5.5"] = open_amber_v5_5_user_chain(path_amber);
     tracks["Edwin"] = open_edwin_user_chain(path_edwin);
 
-    std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>>> correlations = compute_correlations(tracks);
+    // std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>>> correlations = compute_correlations(tracks);
+    std::pair<std::map<std::string, std::vector<double>>, std::map<std::string, std::vector<double>>> correlations = compute_global_correlations(tracks);
     const std::map<std::string, std::vector<double>>& angles = correlations.first;
     const std::map<std::string, std::vector<double>>& distances = correlations.second;
 
