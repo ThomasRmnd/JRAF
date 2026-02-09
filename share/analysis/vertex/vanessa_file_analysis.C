6ccb93242b1a3775f1fed5d467b05525626ca73a
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-09 22:53:37 +0100

    Try

--- share/analysis/vertex/vanessa_file_analysis.C
+++ share/analysis/vertex/vanessa_file_analysis.C
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -5,6 +6,7 @@
 #include <TFile.h>
 #include <TH1D.h>
 #include <TTree.h>
+#include <TVector3.h>
 
 std::vector<double> generate_segment_boundaries(double start, double stop, int num_bins) {
     if (num_bins <= 0) return {};
@@ -253,6 +255,17 @@ int vanessa_file_analysis(const char* filepath) {
     TH1D* h_e_p = new TH1D("h_e_p", "Prompt energy;E_{p} (MeV);Entries;", e_p_bins.size() - 1, e_p_bins.data());
     for (long k = 0l; k < tree->GetEntries(); ++k) {
         tree->GetEntry(k);
+        
+        if (energy_p < 0.7 || 12.0 < energy_p) continue;
+        if (energy_d < 2.0 || 2.5 < energy_d) continue;
+        
+        TVector3 pos_p(x_p, y_p, z_p);
+        TVector3 pos_d(x_d, y_d, z_d);
+        if (16500.0 < pos_p.Mag()) continue;
+        if (16500.0 < pos_d.Mag()) continue;
+        if (15500.0 < pos_p.Z() && pos_p.X() * pos_p.X() + pos_p.Y() * pos_p.Y() < 2000.0 * 2000.0) continue;
+        if (15500.0 < pos_d.Z() && pos_d.X() * pos_d.X() + pos_d.Y() * pos_d.Y() < 2000.0 * 2000.0) continue;
+        
         h_e_p->Fill(energy_p);
     }
 
