78055ef4f16d5b647a0aea55f1ec1b33e33500e8
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-09 23:00:52 +0100

    Try

--- share/analysis/vertex/vanessa_file_analysis.C
+++ share/analysis/vertex/vanessa_file_analysis.C
@@ -268,6 +268,9 @@ int vanessa_file_analysis(const char* filepath) {
 
         if (dt < 5000 || 1000000 < dt) continue;
         if (1.5 < dR) continue;
+
+        std::cout << static_cast<int>(in_neu_veto_p) << ' ' << static_cast<int>(in_neu_veto_d) << ' ' << static_cast<int>(is_flasher_p) << ' ' << static_cast<int>(is_flasher_d) << ' ' << mult_before << ' ' << mult_between << ' ' << mult_after << '\n';
+
         
         h_e_p->Fill(energy_p);
     }
