39fa1eec5871fa6f215875f45f0fd7143124d742
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-18 16:24:31 +0100

    Try

--- share/analysis/vertex/analysis/cosmo_shape_analysis.hpp
+++ share/analysis/vertex/analysis/cosmo_shape_analysis.hpp
@@ -65,16 +65,17 @@ public:
         nb_muons_in_cd_event.fill(m_nav, "CdClassify");
         nb_muons_in_wp_event.fill(m_nav, "WpBasic");
 
-        stopping_muon_lookup has_stopping_in_cd_event;
+        // stopping_muon_lookup has_stopping_in_cd_event;
         stopping_muon_lookup has_stopping_in_wp_event;
-        has_stopping_in_cd_event.fill(m_nav, "CdClassify");
+        // has_stopping_in_cd_event.fill(m_nav, "CdClassify");
         has_stopping_in_wp_event.fill(m_nav, "WpBasic");
 
         for (std::size_t k = 0ul; k < m_nav->method_mu.size(); ++k) {
             if (m_nav->method_mu[k] != m_recname) continue;
+            if (m_nav->totq_cd_mu[k] <= 0.0) continue;
             timestamp ts_mu{m_nav->sec_mu[k], m_nav->nsec_mu[k]};
             if (nb_muons_in_cd_event[ts_mu] > 2ul || nb_muons_in_wp_event[ts_mu] > 2ul) continue;
-            if (has_stopping_in_wp_event[ts_mu] || has_stopping_in_cd_event[ts_mu]) continue;
+            if (has_stopping_in_wp_event[ts_mu]) continue;
 
             bool is_in_bkg = (
                 ts_mu + m_ts_bkg_low <= m_nav->prompt.ts && m_nav->prompt.ts <= ts_mu + m_ts_bkg_high &&
