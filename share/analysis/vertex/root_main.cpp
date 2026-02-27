499e3a6e772aa1d7f7327b7556c3814bcfdbd4bb
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-02-27 15:37:07 +0100

    Try

--- share/analysis/vertex/root_main.cpp
+++ share/analysis/vertex/root_main.cpp
@@ -125,6 +125,9 @@ int root_main(const std::string& filepath) {
     std::shared_ptr<analysis_base> cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex(new cosmo_shape_muon_standard_analysis("cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex", filepath, suffix, "CdWpTtChi2", timestamp{0, 5000000}, timestamp{0, 1200000000}, timestamp{0, -1200000000}, timestamp{0, -5000000}, 3000.0));
     if (!registry.book(cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex)) return 1;
 
+    std::shared_ptr<analysis_base> cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_2s_omilrec_jvertex(new cosmo_shape_muon_standard_analysis("cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_2s_omilrec_jvertex", filepath, suffix, "CdWpTtChi2", timestamp{0, 5000000}, timestamp{2, 0}, timestamp{-2, 0}, timestamp{0, -5000000}, 3000.0));
+    if (!registry.book(cosmo_shape_muon_standard_analysis_cdwpttchi2_3m_2s_omilrec_jvertex)) return 1;
+
     std::shared_ptr<analysis_base> cosmo_shape_muon_with_neutron_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex(new cosmo_shape_muon_with_neutron_analysis("cosmo_shape_muon_with_neutron_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex", filepath, suffix, "CdWpTtChi2", timestamp{0, 5000000}, timestamp{0, 1200000000}, timestamp{0, -1200000000}, timestamp{0, -5000000}, 3000.0));
     if (!registry.book(cosmo_shape_muon_with_neutron_analysis_cdwpttchi2_3m_1_2s_omilrec_jvertex)) return 1;
 
