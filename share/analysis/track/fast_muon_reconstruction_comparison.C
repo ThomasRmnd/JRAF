4110a3256728b97a4eda3a4eef0481f07c1d7d91
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-03-05 11:03:07 +0100

    Try

--- share/analysis/track/fast_muon_reconstruction_comparison.C
+++ share/analysis/track/fast_muon_reconstruction_comparison.C
@@ -908,7 +908,7 @@ int fast_muon_reconstruction_comparison(const char* path_joint, const char* path
         }
     }
     h_classification_single_wp_bundle_amber->GetXaxis()->SetTitle("L (m)");
-    h_classification_single_wp_bundle_amber->GetYaxis()->SetTitel("Entries");
+    h_classification_single_wp_bundle_amber->GetYaxis()->SetTitle("Entries");
     h_classification_single_wp_bundle_amber->Draw();
     c_classification_single_wp_bundle_amber->Update();
 
