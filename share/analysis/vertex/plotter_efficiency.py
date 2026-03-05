efdad3c127f1b1ac8c2c4e90a95cf03a97fd3529
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-03-05 10:06:36 +0100

    Try

--- share/analysis/vertex/plotter_efficiency.py
+++ share/analysis/vertex/plotter_efficiency.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
+import datetime
 import math
-import time
 import time as pytime
 from typing import Tuple
 
@@ -50,6 +50,52 @@ def set_latex_style():
         "savefig.dpi": 300,
 
     })
+
+class timestamp:
+    def __init__(self, sec : int = 0, nsec : int = 0):
+        self.sec = sec
+        self.nsec = nsec
+        self._normalize()
+
+    def __add__(self, other) -> "timestamp":
+        return timestamp(self.sec + other.sec, self.nsec + other.nsec)
+
+    def __sub__(self, other) -> "timestamp":
+        return timestamp(self.sec - other.sec, self.nsec - other.nsec)
+    
+    def __lt__(self, other) -> bool:
+        return self.sec < other.sec or (self.sec == other.sec and self.nsec < other.nsec)
+    
+    def __le__(self, other) -> bool:
+        return self.sec < other.sec or (self.sec == other.sec and self.nsec <= other.nsec)
+    
+    def __gt__(self, other) -> bool:
+        return self.sec > other.sec or (self.sec == other.sec and self.nsec > other.nsec)
+    
+    def __ge__(self, other) -> bool:
+        return self.sec > other.sec or (self.sec == other.sec and self.nsec >= other.nsec)
+
+    def __eq__(self, other) -> bool:
+        return self.sec == other.sec and self.nsec == other.nsec
+
+    def __ne__(self, other) -> bool:
+        return self.sec != other.sec or self.nsec != other.nsec
+    
+    def __str__(self) -> str:
+        dt = datetime.fromtimestamp(self.sec)
+        date_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{date_str}.{self.nsec:09d}"
+    
+    def to_sec(self) -> float:
+        return self.sec + self.nsec / 1e9
+    
+    def to_nsec(self) -> float:
+        return self.sec * 1e9 + self.nsec
+
+    def _normalize(self):
+        carry = self.nsec // 1e9
+        self.sec += carry
+        self.nsec %= 1e9
 
 def generate_grid_points_in_sphere(radius : float, spacing : float, maxpoints : int = None) -> np.ndarray:
     coords = np.arange(-radius, radius, spacing)
