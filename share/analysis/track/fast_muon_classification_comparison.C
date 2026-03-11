fe58dde2987ab22f1fd06c2c05301a39e2c18ce5
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-03-11 17:05:58 +0100

    Try

--- share/analysis/track/fast_muon_classification_comparison.C
+++ share/analysis/track/fast_muon_classification_comparison.C
@@ -1,69 +1,33 @@
 #include <cstdint>
 #include <iostream>
+#include <limits>
+#include <unordered_map>
 
 #include <TFile.h>
 #include <TTimeStamp.h>
 #include <TTree.h>
 #include <TVector3.h>
 
-struct classification {
-
-    int run_id;
-    TTimeStamp ts;
-    double totq_cd;
-    double totq_wp;
-    std::size_t ntracks;
-
-};
-
-bool operator<(const classification& a, const classification& b) {
-    return a.ts < b.ts;
+TF1* fit_rate(TH1D* h) {
+    TF1* f = new TF1(Form("f_%s", h->GetName()), "[0] * exp(-[1] * x)", h->GetXaxis()->GetXmin(), h->GetXaxis()->GetXmax());
+    f->SetParameter(0, h->GetBinContent(1));
+    f->SetParameter(1, h->GetRMS());
+    h->Fit(f, "R");
+    return f;
 }
 
-std::set<classification> open_amber_v5_5_user_chain(const char* path) {
-    TChain* chain = new TChain("MuonReco");
-    chain->Add(path);
-    std::set<classification> classifications;
-
-    int runID;
-    int eventID;
-    int fSec;
-    int fNanoSec;
-    int muonType;
-    float xin, yin, zin;
-    float xout, yout, zout;
-    float charge;
-    chain->SetBranchAddress("runID", &runID);
-    chain->SetBranchAddress("eventID", &eventID);
-    chain->SetBranchAddress("fSec", &fSec);
-    chain->SetBranchAddress("fNanoSec", &fNanoSec);
-    chain->SetBranchAddress("muonType", &muonType);
-    chain->SetBranchAddress("xin", &xin);
-    chain->SetBranchAddress("yin", &yin);
-    chain->SetBranchAddress("zin", &zin);
-    chain->SetBranchAddress("xout", &xout);
-    chain->SetBranchAddress("yout", &yout);
-    chain->SetBranchAddress("zout", &zout);
-
-    long nentries = chain->GetEntries();
-    std::cout << "Info: Found " << nentries << " entries in Amber_v5.5 files\n";
-    for (long k = 0l; k < nentries; ++k) {
-        chain->GetEntry(k);
-        classifications.insert(classification{
-            .run_id = runID,
-            .ts = TTimeStamp(fSec, fNanoSec),
-            .totq_cd = 0.0,
-            .totq_wp = charge,
-            .ntracks = (muonType == 0 ? 1ul : 2ul)
-        });
-    }
-    return classifications;
-}
-
-std::map<std::string, std::set<classification>> open_joint_reco_user_chain(const char* path) {
+int fast_muon_classification_comparison(const char* filepath) {
     TChain* chain = new TChain("muons");
+    if (!chain) {
+        std::cerr << "Cannot create chain muons\n";
+        return 1;
+    }
     chain->Add(path);
-    std::map<std::string, std::set<classification>> classifications;
+    TTree* tree = file->Get<TTree>("muons");
+    if (!tree) {
+        std::cerr << "Cannot retrieve tree muons in file " << filepath << '\n';
+        return 1;
+    }
 
     int run_id;
     time_t sec;
@@ -90,10 +54,22 @@ std::map<std::string, std::set<classification>> open_joint_reco_user_chain(const
     chain->SetBranchAddress("fposy", &fposy);
     chain->SetBranchAddress("fposz", &fposz);
 
-    long nentries = chain->GetEntries();
+    std::unordered_map<int, TH1D*> h_time_to_previous_muon_single;
+    std::unordered_map<int, TH1D*> h_time_to_previous_muon_bundle;
+    std::unordered_map<int, TTimeStamp> prvts_single;
+    std::unordered_map<int, TTimeStamp> prvts_bundle;
+    std::unordered_map<int, TF1*> fit_res_single;
+    std::unordered_map<int, TF1*> fit_res_bundle;
+
+    int min_run_id = std::numeric_limits<int>::max();
+    int max_run_id = std::numeric_limits<int>::min();
+
+    Long64_t nentries = chain->GetEntries();
     std::cout << "Info: Found " << nentries << " entries in joint reco files\n";
-    for (long k = 0l; k < nentries; ++k) {
+    for (Long64_t k = 0l; k < nentries; ++k) {
         chain->GetEntry(k);
+        min_run_id = std::min(min_run_id, run_id);
+        max_run_id = std::max(max_run_id, run_id);
         std::size_t ntracks_cdclassify = 0ul;
         std::size_t ntracks_wpclassify = 0ul;
         for (std::size_t i = 0ul; i < method->size(); ++i) {
@@ -104,123 +80,78 @@ std::map<std::string, std::set<classification>> open_joint_reco_user_chain(const
                 ++ntracks_wpclassify;
             }
         }
-        for (std::size_t i = 0ul; i < method->size(); ++i) {
-            if ((*method)[i] != "CdClassify" && (*method)[i] != "WpBasic") continue;
-            classifications[(*method)[i]].insert(classification{
-                .run_id = run_id,
-                .ts = TTimeStamp(sec, nsec),
-                .totq_cd = totq_cd,
-                .totq_wp = totq_wp,
-                .ntracks = (*method)[i] == "CdClassify" ? ntracks_cdclassify : ntracks_wpclassify
-            });
-        }
-    }
-    return classifications;
-}
-
-
-struct MuonClassification {
-    int run_id;
-    std::size_t ntracks_wpclassify;
-    std::size_t ntracks_cdclassify;
-    std::size_t ntracks_amber;
-};
-
-std::vector<MuonClassification> compute_global_correlations_classification(std::map<std::string, std::set<classification>>& classifications) {
-    if (classifications.find("WpBasic") == classifications.end()) {
-        std::cerr << "Error: WpBasic classifications not found in map\n";
-        return {};
-    }
-    const std::set<classification>& wp_classifications = classifications["WpBasic"];
-
-    std::vector<MuonClassification> results;
-
-    for (const classification& wp_clas : wp_classifications) {
-        std::map<std::string, classification> coincident_map;
-        std::size_t ntracks_cdclassify = 0ul;
-        std::size_t ntracks_amber = 0ul;
-        bool all_found = true;
-
-        for (const auto& [method, clas_set] : classifications) {
-            if (method == "WpBasic") continue;
-            bool found_in_method = false;
-            TTimeStamp lower_bound_ts(wp_clas.ts.GetSec(), wp_clas.ts.GetNanoSec() - 1000);
-            TTimeStamp upper_bound_ts(wp_clas.ts.GetSec(), wp_clas.ts.GetNanoSec() + 1000);
-            std::set<classification>::const_iterator it = clas_set.lower_bound({0, lower_bound_ts, 0.0, 0.0, 0ul});
-            
-            while (it != clas_set.end() && lower_bound_ts <= it->ts && it->ts <= upper_bound_ts) {
-                coincident_map[method] = *it;
-                found_in_method = true;
-                if (method == "CdClassify") {
-                    ntracks_cdclassify = it->ntracks;
-                }
-                if (method == "Amber_v5.5") {
-                    ++ntracks_amber = it->ntracks;
-                }
-                ++it;
+        if (ntracks_wpclassify == 1) {
+            if (prvts_single.find(run_id) == prvts_single.end()) {
+                prvts_single[run_id] = TTimeStamp{sec, nsec};
+                h_time_to_previous_muon_single[run_id] = new TH1D(Form("h_time_to_previous_muon_single_%d", run_id), Form("Time to previous muon for run %d; #Delta t (s); Entries;", run_id), 100, 0.0, 5.0);
+                continue;
             }
-
-            if (!found_in_method) {
-                all_found = false;
-                break; 
+            TTimeStamp ts{sec, nsec};
+            h_time_to_previous_muon_single[run_id]->Fill(ts - prvts_single[run_id]);
+            prvts_single[run_id] = ts;
+        }
+        if (ntracks_wpclassify > 1) {
+            if (prvts_bundle.find(run_id) == prvts_bundle.end()) {
+                prvts_bundle[run_id] = TTimeStamp{sec, nsec};
+                h_time_to_previous_muon_bundle[run_id] = new TH1D(Form("h_time_to_previous_muon_bundle_%d", run_id), Form("Time to previous muon for run %d; #Delta t (s); Entries;", run_id), 100, 0.0, 5.0);
+                continue;
             }
-        }
-
-        if (all_found) {
-            results.push_back(MuonClassification{
-                .run_id = wp_clas.run_id,
-                .ntracks_wpclassify = wp_clas.ntracks,
-                .ntracks_cdclassify = ntracks_cdclassify,
-                .ntracks_amber = ntracks_amber
-            });
-        }
-    }
-    return results;
-}
-
-int fast_muon_classification_comparison(const char* path_joint, const char* path_amber) {
-    std::map<std::string, std::set<classification>> classifications = open_joint_reco_user_chain(path_joint);
-    classifications["Amber_v5.5"] = open_amber_v5_5_user_chain(path_amber);
-
-    std::vector<MuonClassification> results = compute_global_correlations_classification(classifications);
-
-    std::cout << "Results size: " << results.size() << '\n';
-
-    int min_run_id = classifications["WpBasic"].begin()->run_id;
-    int max_run_id = classifications["WpBasic"].rbegin()->run_id;
-    
-    TH1D* h_nb_of_single_per_run_wpclassify = new TH1D("h_nb_of_single_per_run_wpclassify", "Number of single muons per run (WpClassify)", max_run_id - min_run_id + 1, min_run_id, max_run_id + 1);
-    TH1D* h_nb_of_bundle_per_run_wpclassify = new TH1D("h_nb_of_bundle_per_run_wpclassify", "Number of bundle muons per run (WpClassify)", max_run_id - min_run_id + 1, min_run_id, max_run_id + 1);
-    
-    for (const classification& clas : classifications["WpBasic"]) {
-        if (clas.ntracks == 1) {
-            h_nb_of_single_per_run_wpclassify->Fill(clas.run_id);
-        }
-        else {
-            h_nb_of_bundle_per_run_wpclassify->Fill(clas.run_id);
+            TTimeStamp ts{sec, nsec};
+            h_time_to_previous_muon_bundle[run_id]->Fill(ts - prvts_bundle[run_id]);
+            prvts_bundle[run_id] = ts;
         }
     }
 
-    TCanvas* c_per_run_wpclassify = new TCanvas("c_per_run_wpclassify", "Number of single muons per run (WpClassify)", 1000, 1000);
-    c_per_run_wpclassify->cd();
-    h_nb_of_single_per_run_wpclassify->SetStats(0);
-    h_nb_of_single_per_run_wpclassify->GetXaxis()->SetTitle("Run ID");
-    h_nb_of_single_per_run_wpclassify->GetYaxis()->SetTitle("Entries");
-    h_nb_of_single_per_run_wpclassify->SetMarkerStyle(kFullCircle);
-    h_nb_of_single_per_run_wpclassify->SetMarkerColor(kBlue);
-    h_nb_of_single_per_run_wpclassify->SetMarkerSize(2.0);
-    h_nb_of_single_per_run_wpclassify->Draw();
-    h_nb_of_bundle_per_run_wpclassify->SetStats(0);
-    h_nb_of_bundle_per_run_wpclassify->GetXaxis()->SetTitle("Run ID");
-    h_nb_of_bundle_per_run_wpclassify->GetYaxis()->SetTitle("Entries");
-    h_nb_of_bundle_per_run_wpclassify->SetMarkerStyle(kFullCircle);
-    h_nb_of_bundle_per_run_wpclassify->SetMarkerColor(kRed);
-    h_nb_of_bundle_per_run_wpclassify->SetMarkerSize(2.0);
-    h_nb_of_bundle_per_run_wpclassify->Draw("SAME");
-    c_per_run_wpclassify->SetTickx();
-    c_per_run_wpclassify->SetTicky();
-    c_per_run_wpclassify->SetGrid();
-    c_per_run_wpclassify->Update();
+    for (const auto& [run_id, h] : h_time_to_previous_muon_single) {
+        fit_res_single[run_id] = fit_rate(h);
+    }
+    for (const auto& [run_id, h] : h_time_to_previous_muon_bundle) {
+        fit_res_bundle[run_id] = fit_rate(h);
+    }
+
+    TH1D* h_rate_per_run_single = new TH1D("h_rate_per_run_single", "Rate per run for single;RUN ID;Rate (cps);", max_run_id - min_run_id + 1, min_run_id, max_run_id + 1);
+    TH1D* h_rate_per_run_bundle = new TH1D("h_rate_per_run_bundle", "Rate per run for bundle;RUN ID;Rate (cps);", max_run_id - min_run_id + 1, min_run_id, max_run_id + 1);
+    for (const auto& [run_id, h] : h_time_to_previous_muon_single) {
+        h_rate_per_run_single->SetBinContent(run_id - min_run_id + 1, fit_res_single[run_id]->GetParameter(1));
+        h_rate_per_run_single->SetBinError(run_id - min_run_id + 1, fit_res_single[run_id]->GetParError(1));
+    }
+    for (const auto& [run_id, h] : h_time_to_previous_muon_bundle) {
+        h_rate_per_run_bundle->SetBinContent(run_id - min_run_id + 1, fit_res_bundle[run_id]->GetParameter(1));
+        h_rate_per_run_bundle->SetBinError(run_id - min_run_id + 1, fit_res_bundle[run_id]->GetParError(1));
+    }
+
+    TCanvas* c_rate_per_run = new TCanvas("c_rate_per_run", "Rate per run", 1000, 1000);
+    c_rate_per_run->cd();
+    h_rate_per_run_single->SetStats(0);
+    h_rate_per_run_single->GetYaxis()->SetMaxDigits(3);
+    h_rate_per_run_single->GetXaxis()->CenterTitle(true);
+    h_rate_per_run_single->GetYaxis()->CenterTitle(true);
+    h_rate_per_run_single->GetYaxis()->SetTitleOffset(1.25);
+    h_rate_per_run_single->SetMarkerStyle(kFullCircle);
+    h_rate_per_run_single->SetMarkerColor(kBlue);
+    h_rate_per_run_single->SetMarkerSize(1.0);
+    h_rate_per_run_single->SetLineWidth(2);
+    h_rate_per_run_single->SetLineColor(kBlue);
+    h_rate_per_run_single->SetMinimum(0.0);
+    h_rate_per_run_single->Draw();
+    c_rate_per_run->Update();
+    h_rate_per_run_bundle->SetStats(0);
+    h_rate_per_run_bundle->GetYaxis()->SetMaxDigits(3);
+    h_rate_per_run_bundle->GetXaxis()->CenterTitle(true);
+    h_rate_per_run_bundle->GetYaxis()->CenterTitle(true);
+    h_rate_per_run_bundle->GetYaxis()->SetTitleOffset(1.25);
+    h_rate_per_run_bundle->SetMarkerStyle(kFullCircle);
+    h_rate_per_run_bundle->SetMarkerColor(kRed);
+    h_rate_per_run_bundle->SetMarkerSize(1.0);
+    h_rate_per_run_bundle->SetLineWidth(2);
+    h_rate_per_run_bundle->SetLineColor(kRed);
+    h_rate_per_run_bundle->SetMinimum(0.0);
+    h_rate_per_run_bundle->Draw("SAME");
+    c_rate_per_run->Update();
+    c_rate_per_run->SetTickx();
+    c_rate_per_run->SetTicky();
+    c_rate_per_run->SetGrid();
+    c_rate_per_run->Update();
 
     return 0;
 }
