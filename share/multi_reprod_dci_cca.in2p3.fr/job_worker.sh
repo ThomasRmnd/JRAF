261ab962cb2c23e5518fbebdbeb0ae286c58a7d2
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-03-24 10:46:04 +0100

    Try

--- share/multi_reprod_dci_cca.in2p3.fr/job_worker.sh
+++ share/multi_reprod_dci_cca.in2p3.fr/job_worker.sh
@@ -440,6 +440,7 @@ main() {
         --output "${local_output_file}" \
         --context-previous-filename "${prev_file_local}" \
         --context-next-filename "${next_file_local}" \
+        --tt-reco-filepath "${tt_reco_filepath}" \
         --reco-output "${local_reco_output_file}" \
         "${EXTRA_ARGS[@]}"
         # --tt-reco-filepath "${tt_reco_filepath}" \
